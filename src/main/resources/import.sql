292578aaf73a29d6e2f813599ddf38067c1680c8
Mushfikunnabi Nijhum <m.nijhum0@gmail.com>
2022-03-20 21:27:52 +0600

    initial database added

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO parent(first_name, last_name, street_address, city, state, zip) VALUES ('John', 'Snow','Shahbag', 'Dhaka', 'Dhaka', '1210');
+INSERT INTO parent(first_name, last_name, street_address, city, state, zip) VALUES ('Daenarys', 'Targerian','Mohakhali', 'Dhaka', 'Dhaka', '1212');
+INSERT INTO child(first_name, last_name,parent_id) VALUES ('test', 'name1', 1);
+INSERT INTO child(first_name, last_name,parent_id) VALUES ('test', 'name2',2);
